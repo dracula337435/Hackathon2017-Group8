b094a28cd4dbe16f834000418a4b4c14efb88a09
dracula337435 <dracula337435@163.com>
2017-12-07 23:42:31 +0800

    优化启动脚本。之前用pwd，造成只能在bin目录下操作。现在改用pwd+sh后的参数，得到完整的脚本路径，再继续

--- portal/src/main/resources/bin/start.sh
+++ portal/src/main/resources/bin/start.sh
@@ -1,31 +1,30 @@
 #启动脚本
 #!/bin/bash
 
+#之前用pwd，造成只能在bin目录下操作。现在改用pwd+sh后的参数，得到完整的脚本路径，再继续
+SCRIPT_PATH="`pwd`/$0"
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
+
 #当前目录应该是bin（这样就一定不会是/），否则不能往上走
-CURRENT_DIR=`pwd`
-BIN_DIR=`pwd |awk -F "/" '{print $NF}'`
+BIN_DIR=`echo $SCRIPT_DIR |awk -F "/" '{print $NF}'`
 if [ $BIN_DIR != "bin" ]; then
-	echo "当前目录为$CURRENT_DIR，不为bin，app部署有误"
+	echo "启动脚本所在目录为$SCRIPT_DIR，不为bin，app部署有误"
 	exit 1
 fi
-BIN_DIR=`pwd`
+BIN_DIR=$SCRIPT_DIR
 
 #向上一层，应该有conf，lib
-cd ..
-APP_DIR=`pwd`
-if [ ! -d conf ]; then
+APP_DIR="$BIN_DIR/.."
+CONF_DIR="$APP_DIR/conf"
+if [ ! -d "$CONF_DIR" ]; then
     echo "部署目录下没有文件夹conf，app部署有误，部署目录为$APP_DIR"
     exit 2
 fi
-cd conf
-CONF_DIR=`pwd`
-cd ..
-if [ ! -d lib ]; then
+LIB_DIR="$APP_DIR/lib"
+if [ ! -d "$LIB_DIR" ]; then
     echo "部署目录下没有文件夹lib，app部署有误，部署目录为$APP_DIR"
     exit 3
 fi
-cd lib
-LIB_DIR=`pwd`
 
 #有java命令可用
 if [ -z `which java` ]; then
@@ -35,8 +34,8 @@ fi
 
 #classpath中加上conf
 #凑出lib下的jar包为classpath
-CLASSPATH=../conf/
-for i in *.jar
+CLASSPATH="$CONF_DIR/"
+for i in $LIB_DIR/*.jar
 do
     CLASSPATH="$CLASSPATH":"$i"
 done
