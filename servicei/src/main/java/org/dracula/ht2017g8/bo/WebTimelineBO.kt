dd34256af93f7be10057dcd70378f354ac44a697
dracula337435 <dracula337435@163.com>
2017-12-13 23:30:13 +0800

    web_timeline的BO由java换kotlin

---
+++ servicei/src/main/java/org/dracula/ht2017g8/bo/WebTimelineBO.kt
@@ -0,0 +1,18 @@
+package org.dracula.ht2017g8.bo
+
+class WebTimelineBO{
+
+    var id: Int? = null
+
+    var custnm: String? = null
+
+    var custid: String? = null
+
+    var datetime: String? = null
+
+    var channel: String? = null
+
+    var action: String? = null
+
+    var acttype: String? = null
+}
