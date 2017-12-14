6251242b39e4244b599809356fbcd5c1a60a9223
dracula337435 <dracula337435@163.com>
2017-12-14 21:20:19 +0800

    启用spring-boot-admin

--- portal/src/main/java/org/dracula/ht2017g8/MainApp.java
+++ portal/src/main/java/org/dracula/ht2017g8/MainApp.java
@@ -1,11 +1,13 @@
 package org.dracula.ht2017g8;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
+@EnableAdminServer
 public class MainApp {
 
     public static void main(String[] args){
