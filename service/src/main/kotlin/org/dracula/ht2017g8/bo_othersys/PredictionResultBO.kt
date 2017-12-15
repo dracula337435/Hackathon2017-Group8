72bdc73bd9a37b296d8ef8494e4f323df776f254
dracula337435 <dracula337435@163.com>
2017-12-15 20:12:56 +0800

    kotlin文件进kotlin文件夹

---
+++ service/src/main/kotlin/org/dracula/ht2017g8/bo_othersys/PredictionResultBO.kt
@@ -0,0 +1,17 @@
+package org.dracula.ht2017g8.bo_othersys
+
+class PredictionResultBO{
+
+    var label: String? =null
+
+    var features: List<String>? =null
+
+    var rawPrediction: List<String>? =null
+
+    var probability: List<String>? = null
+
+    var prediction: String? =null
+
+    var predictionLabel: String? =null
+
+}
