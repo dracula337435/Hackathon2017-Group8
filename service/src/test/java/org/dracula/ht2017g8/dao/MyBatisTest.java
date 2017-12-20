7e70159d0e4e1df471a34350d3eb948a4af9e8ad
dracula337435 <dracula337435@163.com>
2017-12-20 10:57:32 +0800

    测试类也用上spring的profile

--- service/src/test/java/org/dracula/ht2017g8/dao/MyBatisTest.java
+++ service/src/test/java/org/dracula/ht2017g8/dao/MyBatisTest.java
@@ -9,13 +9,17 @@ import org.dracula.ht2017g8.po.mybatis.WebTimeline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={MyBatisConfig.class, DataSourceEmbeddedH2.class})
+@ContextConfiguration(classes={MyBatisConfig.class,
+                                DataSourceDruidAndMysql.class,
+                                DataSourceEmbeddedH2.class})
+@ActiveProfiles("DataSourceEmbeddedH2")
 public class MyBatisTest {
 
     @Autowired
--- service/src/test/java/org/dracula/ht2017g8/dao/SpringDataJPATest.java
+++ service/src/test/java/org/dracula/ht2017g8/dao/SpringDataJPATest.java
@@ -5,13 +5,17 @@ import org.dracula.ht2017g8.po.jpa.WebCustomer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SpringDataJPAConfig.class, DataSourceEmbeddedH2.class})
+@ContextConfiguration(classes={SpringDataJPAConfig.class,
+                                DataSourceDruidAndMysql.class,
+                                DataSourceEmbeddedH2.class})
+@ActiveProfiles("DataSourceEmbeddedH2")
 public class SpringDataJPATest {
 
     @Autowired
