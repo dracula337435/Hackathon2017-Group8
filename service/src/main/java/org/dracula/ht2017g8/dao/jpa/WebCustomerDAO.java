f5dc68ca21fe126d50b3902d2b1115651334182e
dracula337435 <dracula337435@163.com>
2017-12-29 19:28:30 +0800

    集成进来spring-data-rest

--- service/src/main/java/org/dracula/ht2017g8/dao/jpa/WebCustomerDAO.java
+++ service/src/main/java/org/dracula/ht2017g8/dao/jpa/WebCustomerDAO.java
@@ -2,6 +2,8 @@ package org.dracula.ht2017g8.dao.jpa;
 
 import org.dracula.ht2017g8.po.jpa.WebCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="WebCustomer")
 public interface WebCustomerDAO extends JpaRepository<WebCustomer, Long>{
 }
