3bcfba0ee2752213d4379cdd6fed988b94c57dba
dk <dracula337435@163.com>
2018-03-07 02:57:17 +0800

    升级spring-boot到2.0.0，发现启动有问题，找原因未果。笨方法，逐个禁用后试验，发现是spring-boot-admin的问题，于是禁用之。若有server（无论是否有client），则报最开始的错误，hibernate-validator相关；若仅有client，则报一个找不到类定义的错

--- portal/src/main/java/org/dracula/ht2017g8/MainApp.java
+++ portal/src/main/java/org/dracula/ht2017g8/MainApp.java
@@ -1,13 +1,13 @@
 package org.dracula.ht2017g8;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+//import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
-@EnableAdminServer
+//@EnableAdminServer
 public class MainApp {
 
     public static void main(String[] args){
